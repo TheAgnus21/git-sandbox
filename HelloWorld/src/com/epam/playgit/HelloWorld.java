21dc8469ed9fefdc80f56e07d48ebf9b7c248ee4
TheAgnus21 <judzin.baranovsky@gmail.com>
2014-05-08 10:53:48 +0300

    Update HelloWorld.java

--- HelloWorld/src/com/epam/playgit/HelloWorld.java
+++ HelloWorld/src/com/epam/playgit/HelloWorld.java
@@ -3,6 +3,7 @@ package com.epam.playgit;
 public class HelloWorld {
 
     public static void main(final String[] arguments) {
+        // Comment
         System.out.println("Hello, World!");
     }
 
